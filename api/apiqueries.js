622a639524bdef55d39467f527231964a11c251b
Cedric Mensah <cedric@mensah.de>
2020-07-27 23:38:26 +0200

    Pausing API calls at 90% rate limit to avoid throttling

--- api/apiqueries.js
+++ api/apiqueries.js
@@ -87,6 +87,7 @@ async function processData(url, req) {
 
   let response = await api.get(url).catch(err => {
 
+
     if (err.message.includes("400") == true) {
 
       if (err.response.data.error.code == 613) {
@@ -105,9 +106,6 @@ async function processData(url, req) {
         console.log("The Facebook API request failed because of invalid parameters")
         return;
       }
-
-
-
     }
   })
 
@@ -117,128 +115,142 @@ async function processData(url, req) {
   let processedData = []
   let apiUsage = JSON.parse(response.headers['x-business-use-case-usage'])
 
-  response.data.data.forEach(element => {
 
-    let post = {};
-    let generated_id = unique_id.generate();
 
-    if (typeof element.demographic_distribution === "undefined") {
-      id = generated_id;
-    } else {
-      element.demographic_distribution.forEach(item => {
-        item.id = generated_id;
-        item.ad_snapshot_url = element.ad_snapshot_url;
-      });
-    }
+  if (apiUsage['2381529675200446'][0].object_count_pct > 90) {
+    await pauseAPICalls()
+    processData(url, req)
 
-    if (typeof element.region_distribution === "undefined") {
-      id = generated_id;
-    } else {
-      element.region_distribution.forEach(item => {
-        item.id = generated_id;
-        item.ad_snapshot_url = element.ad_snapshot_url;
-      });
-    }
+  } else {
+    response.data.data.forEach(element => {
 
-    post.ad_snapshot_url = element.ad_snapshot_url;
-    post.id = generated_id;
-    post.page_name = element.page_name;
-    post.page_id = element.page_id;
-    post.funding_entity = element.funding_entity;
-    if (typeof element.impressions === "undefined") {
-      post.impressions_lower_bound = null;
-    } else {
-      if (typeof element.impressions.lower_bound === "undefined") {
+      let post = {};
+      let generated_id = unique_id.generate();
+
+      if (typeof element.demographic_distribution === "undefined") {
+        id = generated_id;
+      } else {
+        element.demographic_distribution.forEach(item => {
+          item.id = generated_id;
+          item.ad_snapshot_url = element.ad_snapshot_url;
+        });
+      }
+
+      if (typeof element.region_distribution === "undefined") {
+        id = generated_id;
+      } else {
+        element.region_distribution.forEach(item => {
+          item.id = generated_id;
+          item.ad_snapshot_url = element.ad_snapshot_url;
+        });
+      }
+
+      post.ad_snapshot_url = element.ad_snapshot_url;
+      post.id = generated_id;
+      post.page_name = element.page_name;
+      post.page_id = element.page_id;
+      post.funding_entity = element.funding_entity;
+      if (typeof element.impressions === "undefined") {
         post.impressions_lower_bound = null;
       } else {
-        post.impressions_lower_bound = element.impressions.lower_bound;
+        if (typeof element.impressions.lower_bound === "undefined") {
+          post.impressions_lower_bound = null;
+        } else {
+          post.impressions_lower_bound = element.impressions.lower_bound;
+        }
       }
-    }
-    if (typeof element.impressions === "undefined") {
-      post.impressions_upper_bound = null;
-    } else {
-      if (typeof element.impressions.upper_bound === " undefined") {
+      if (typeof element.impressions === "undefined") {
         post.impressions_upper_bound = null;
       } else {
-        post.impressions_upper_bound = element.impressions.upper_bound;
+        if (typeof element.impressions.upper_bound === " undefined") {
+          post.impressions_upper_bound = null;
+        } else {
+          post.impressions_upper_bound = element.impressions.upper_bound;
+        }
       }
-    }
-    if (typeof element.spend === "undefined") {
-      post.spend_lower_bound = null;
-      post.spend_upper_bound = null;
-    } else {
-      if (typeof element.spend.lower_bound === "undefined") {
+      if (typeof element.spend === "undefined") {
         post.spend_lower_bound = null;
-      } else {
-        post.spend_lower_bound = element.spend.lower_bound;
-      }
-      if (typeof element.spend.upper_bound === "undefined") {
         post.spend_upper_bound = null;
       } else {
-        post.spend_upper_bound = element.spend.upper_bound;
+        if (typeof element.spend.lower_bound === "undefined") {
+          post.spend_lower_bound = null;
+        } else {
+          post.spend_lower_bound = element.spend.lower_bound;
+        }
+        if (typeof element.spend.upper_bound === "undefined") {
+          post.spend_upper_bound = null;
+        } else {
+          post.spend_upper_bound = element.spend.upper_bound;
+        }
       }
-    }
 
-    post.currency = element.currency;
-    if (typeof element.demographic_distribution === "undefined") {
-      post.demographic_distribution = {
-        id: generated_id,
-        percentage: "",
-        age: "",
-        gender: "",
-        ad_snapshot_url: ""
-      };
-    } else {
-      post.demographic_distribution = element.demographic_distribution;
-    }
-    if (typeof element.region_distribution === "undefined") {
-      post.region_distribution = {
-        id: generated_id,
-        percentage: "",
-        region: "",
-        ad_snapshot_url: ""
-      };
-    } else {
-      post.region_distribution = element.region_distribution;
-    }
-    post.ad_creative_body = JSON.stringify(element.ad_creative_body);
-    post.ad_creative_link_caption = JSON.stringify(element.ad_creative_link_caption);
-    post.ad_creative_link_description = JSON.stringify(element.ad_creative_link_description);
-    post.ad_creative_link_title = JSON.stringify(element.ad_creative_link_title);
-    post.ad_creation_time = element.ad_creation_time
-      .replace("T", " ")
-      .replace("+0000", "");
-    if (typeof element.ad_delivery_start_time === "undefined") {
-      post.ad_delivery_start_time = null;
-    } else {
-      post.ad_delivery_start_time = element.ad_delivery_start_time
+      post.currency = element.currency;
+      if (typeof element.demographic_distribution === "undefined") {
+        post.demographic_distribution = {
+          id: generated_id,
+          percentage: "",
+          age: "",
+          gender: "",
+          ad_snapshot_url: ""
+        };
+      } else {
+        post.demographic_distribution = element.demographic_distribution;
+      }
+      if (typeof element.region_distribution === "undefined") {
+        post.region_distribution = {
+          id: generated_id,
+          percentage: "",
+          region: "",
+          ad_snapshot_url: ""
+        };
+      } else {
+        post.region_distribution = element.region_distribution;
+      }
+      post.ad_creative_body = JSON.stringify(element.ad_creative_body);
+      post.ad_creative_link_caption = JSON.stringify(element.ad_creative_link_caption);
+      post.ad_creative_link_description = JSON.stringify(element.ad_creative_link_description);
+      post.ad_creative_link_title = JSON.stringify(element.ad_creative_link_title);
+      post.ad_creation_time = element.ad_creation_time
         .replace("T", " ")
         .replace("+0000", "");
-    }
-    if (typeof element.ad_delivery_stop_time === "undefined") {
-      post.ad_delivery_stop_time = null;
+      if (typeof element.ad_delivery_start_time === "undefined") {
+        post.ad_delivery_start_time = null;
+      } else {
+        post.ad_delivery_start_time = element.ad_delivery_start_time
+          .replace("T", " ")
+          .replace("+0000", "");
+      }
+      if (typeof element.ad_delivery_stop_time === "undefined") {
+        post.ad_delivery_stop_time = null;
+      } else {
+        post.ad_delivery_stop_time = element.ad_delivery_stop_time
+          .replace("T", " ")
+          .replace("+0000", "");
+      }
+      processedData.push(post);
+    })
+    await db.storeInDB(processedData, req).catch(error => console.log(error))
+    console.log(counter + " Pages Processed")
+    console.log("API Rate Limit: " + apiUsage['2381529675200446'][0].object_count_pct + "% Reached")
+    console.log("Time to Regain Access: " + apiUsage['2381529675200446'][0].estimated_time_to_regain_access + " Minutes")
+    console.log("API CPU Time: " + apiUsage['2381529675200446'][0].total_cputime + "% Reached")
+    console.log("Total API Request Time: " + apiUsage['2381529675200446'][0].total_time + "% Reached")
+
+
+
+    if (response.data.paging.next !== undefined) {
+      processData(response.data.paging.next, persistentReq)
+      counter++
     } else {
-      post.ad_delivery_stop_time = element.ad_delivery_stop_time
-        .replace("T", " ")
-        .replace("+0000", "");
+      console.log("There is no more data")
     }
-    processedData.push(post);
-  })
-  await db.storeInDB(processedData, req).catch(error => console.log(error))
-  console.log(counter + " Pages Processed")
-  console.log("API Rate Limit: " + apiUsage['2381529675200446'][0].object_count_pct + "% Reached")
-  console.log("Time to Regain Access: " + apiUsage['2381529675200446'][0].estimated_time_to_regain_access + " Minutes")
-  console.log("API CPU Time: " + apiUsage['2381529675200446'][0].total_cputime + "% Reached")
-  console.log("Total API Request Time: " + apiUsage['2381529675200446'][0].total_time + "% Reached")
 
 
-
-  if (response.data.paging.next !== undefined) {
-    processData(response.data.paging.next, persistentReq)
-    counter++
-  } else {
-    console.log("There is no more data")
   }
 
+}
 
+
+async function pauseAPICalls() {
+  return new Promise(resolve => setTimeout(resolve, 3600000))
 }
