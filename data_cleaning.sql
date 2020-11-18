79909312dbc38dc9c606e930e24c31d17c5b69b0
Cedric Mensah <cedric@mensah.de>
2020-11-18 11:19:25 +0100

    Added file with data cleaning sql statements

---
+++ data_cleaning.sql
@@ -0,0 +1,452 @@
+UPDATE norway_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE norway_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE norway_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE norway_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE norway_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE norway_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE norway_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE norway_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE norway_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE norway_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE norway_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE norway_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE norway_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE norway_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE australia_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE australia_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE australia_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE australia_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE australia_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE australia_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE australia_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE australia_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE australia_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE australia_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE australia_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE australia_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE australia_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE australia_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE denmark_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE denmark_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE denmark_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE denmark_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE denmark_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE denmark_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE denmark_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE denmark_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE denmark_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE denmark_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE denmark_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE denmark_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE denmark_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE denmark_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE france_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE france_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE france_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE france_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE france_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE france_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE france_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE france_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE france_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE france_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE france_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE france_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE france_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE france_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE finland_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE finland_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE finland_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE finland_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE finland_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE finland_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE finland_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE finland_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE finland_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE finland_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE finland_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE finland_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE finland_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE finland_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE germany_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE germany_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE germany_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE germany_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE germany_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE germany_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE germany_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE germany_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE germany_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE germany_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE germany_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE germany_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE germany_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE germany_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE uk_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE uk_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE uk_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE uk_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE uk_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE uk_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE uk_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE uk_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE uk_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE uk_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE uk_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE uk_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE uk_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE uk_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE us_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE us_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE us_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE us_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE us_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE us_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE us_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE us_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE us_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE us_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE us_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE us_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE us_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE us_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE hungary_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE hungary_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE hungary_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE hungary_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE hungary_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE hungary_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE hungary_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE hungary_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE hungary_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE hungary_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE hungary_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE hungary_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE hungary_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE hungary_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE slovakia_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE slovakia_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE slovakia_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE slovakia_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE slovakia_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE slovakia_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE slovakia_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE slovakia_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE slovakia_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE slovakia_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE slovakia_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE slovakia_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE slovakia_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE slovakia_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE sweden_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n\\n', ' '));
+UPDATE sweden_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\n', ' '));
+UPDATE sweden_posts
+SET ad_creative_link_title = (REPLACE(ad_creative_link_title, '\\"', '""'));
+UPDATE sweden_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n\\n', ' '));
+UPDATE sweden_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\n', ' '));
+UPDATE sweden_posts
+SET ad_creative_link_caption = (REPLACE(ad_creative_link_caption, '\\"', '""'));
+UPDATE sweden_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n\\n', ' ')
+    );
+UPDATE sweden_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\n', ' ')
+    );
+UPDATE sweden_posts
+SET ad_creative_link_description = (
+        REPLACE(ad_creative_link_description, '\\"', '""')
+    );
+UPDATE sweden_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n\\n', ' '));
+UPDATE sweden_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\n', ' '));
+UPDATE sweden_posts
+SET ad_creative_body = (REPLACE(ad_creative_body, '\\"', '""'));
+UPDATE sweden_posts
+SET page_name = (REPLACE(page_name, ';', ','));
+UPDATE sweden_posts
+SET funding_entity = (REPLACE(funding_entity, ';', ','));
+UPDATE australia_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE australia_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE denmark_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE denmark_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE finland_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE finland_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE france_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE france_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE germany_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE germany_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE india_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE india_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE hungary_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE hungary_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE slovakia_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE slovakia_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE uk_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE uk_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE us_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE us_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE sweden_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE sweden_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE norway_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE norway_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
+UPDATE australia_posts
+SET ad_snapshot_url = SUBSTRING_INDEX(ad_snapshot_url, '&access_token=', 1);
+UPDATE australia_posts
+SET ad_snapshot_url = (
+        REPLACE(ad_snapshot_url, '\archive/render_ad', 'library')
+    );
